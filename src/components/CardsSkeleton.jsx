3f3836fe7159f55359604fac0828a1b9b23782dc
Najib Hossain <najibhossn@gmail.com>
2025-06-25 16:25:43 +0600

    Update skeleton width and style

--- src/components/CardsSkeleton.jsx
+++ src/components/CardsSkeleton.jsx
@@ -1,12 +1,14 @@
 
 const CardsSkeleton = () => {
   return (
-    <div className="h-[400px] lg:max-2xl:h-[400px] w-full flex
-     flex-col justify-center items-center gap-5">
-      <div className="skeleton h-6 w-full"></div>
-      <div className="skeleton h-6 w-full"></div>
-      <div className="skeleton h-6 w-full"></div>
-    </div>
+      <>
+      <div className="skeleton h-[160px] w-full bg-zinc-900"></div>
+      <div className="skeleton h-[160px] w-full bg-zinc-900"></div>
+      <div className="skeleton h-[160px] w-full bg-zinc-900"></div>
+      <div className="skeleton h-[160px] w-full bg-zinc-900"></div>
+      <div className="skeleton h-[160px] w-full bg-zinc-900"></div>
+      <div className="skeleton h-[160px] w-full bg-zinc-900"></div>
+      </>
   )
 }
 
