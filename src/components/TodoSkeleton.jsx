04adb4097f39de6429508d3c4beb53e824eb0713
Najib Hossain <najibhossn@gmail.com>
2025-06-25 16:57:20 +0600

    Update skeleton width and style

--- src/components/TodoSkeleton.jsx
+++ src/components/TodoSkeleton.jsx
@@ -1,13 +1,13 @@
 
 const TodoSkeleton = () => {
   return (
-    <div className="h-full w-full flex flex-col justify-center
-     items-center gap-5">
+    <>
+    <div className="h-[200px] w-full flex flex-col justify-center items-center gap-5">
       <div className="skeleton h-5 w-full md:w-[80%] lg:w-[60%] bg-zinc-900"></div>
       <div className="skeleton h-5 w-full md:w-[80%] lg:w-[60%] bg-zinc-900"></div>
       <div className="skeleton h-5 w-full md:w-[80%] lg:w-[60%] bg-zinc-900"></div>
     </div>
-
+    </>
   )
 }
 
