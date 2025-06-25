ecb758127d3b11d52f9ef98358f0cd7dfd56cb82
Najib Hossain <najibhossn@gmail.com>
2025-06-25 16:32:08 +0600

    Update todo skeleton color and height

--- src/components/TodoSkeleton.jsx
+++ src/components/TodoSkeleton.jsx
@@ -3,9 +3,9 @@ const TodoSkeleton = () => {
   return (
     <div className="h-full w-full flex flex-col justify-center
      items-center gap-5">
-      <div className="skeleton h-6 w-full md:w-[80%] lg:w-[60%]"></div>
-      <div className="skeleton h-6 w-full md:w-[80%] lg:w-[60%]"></div>
-      <div className="skeleton h-6 w-full md:w-[80%] lg:w-[60%]"></div>
+      <div className="skeleton h-5 w-full md:w-[80%] lg:w-[60%] bg-zinc-900"></div>
+      <div className="skeleton h-5 w-full md:w-[80%] lg:w-[60%] bg-zinc-900"></div>
+      <div className="skeleton h-5 w-full md:w-[80%] lg:w-[60%] bg-zinc-900"></div>
     </div>
 
   )
