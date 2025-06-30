6e8020f2acf0ee3c3c44223d6fbc2df5cf3bc5b2
Najib Hossain <najibhossn@gmail.com>
2025-06-30 16:37:36 +0600

    Remove loading state and update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { AuthProvider, useAuth } from "./utils/AuthProvider";
+import { ThemeProvider } from "./utils/ThemeProvider";
+import { AuthProvider } from "./utils/AuthProvider";
 import PrivateRoute from "./utils/PrivateRoute"
 import Home from "./pages/Home"
 import SignIn from "./pages/SignIn"
@@ -10,7 +11,6 @@ import NoteView from "./pages/NoteView"
 import TaskManager from "./pages/TaskManager"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import GlobalSkeleton from "./components/GlobalSkeleton"
 import ResourcesManager from "./pages/ResourcesManager";
 import AddNewResource from "./pages/AddNewResource";
 import ResourceView from "./pages/ResourceView";
@@ -19,48 +19,31 @@ const App = () => {
   return (
     <>
     <BrowserRouter>
+      <ThemeProvider>
       <AuthProvider>
       <Header />
       <main className="h-auto w-full">
-        <AuthRoutes /> {/* Moved loading state inside here */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route element={<PrivateRoute />}>
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/notes' element={<NotesManager />} />
+            <Route path='/add-new-note' element={<AddNewNote />} />
+            <Route path='/note-view/:docID' element={<NoteView />} />
+            <Route path='/todos' element={<TaskManager />} />
+            <Route path='/resources' element={<ResourcesManager />} />
+            <Route path='/add-new-resource' element={<AddNewResource />} />
+            <Route path='/resource-view/:docID' element={<ResourceView />} />
+          </Route>
+        </Routes>
       </main>
       <Footer />
       </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
     </>
   )
-}
-
-//Component to handle loading state inside Routes
-const AuthRoutes = () => {
-
-  const {loading} = useAuth();
-
-  if (loading) {
-    return (
-      <>
-      <GlobalSkeleton />
-      </>
-    )
-  }
-
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route element={<PrivateRoute />}>
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/notes' element={<NotesManager />} />
-        <Route path='/add-new-note' element={<AddNewNote />} />
-        <Route path='/note-view/:docID' element={<NoteView />} />
-        <Route path='/todos' element={<TaskManager />} />
-        <Route path='/resources' element={<ResourcesManager />} />
-        <Route path='/add-new-resource' element={<AddNewResource />} />
-        <Route path='/resource-view/:docID' element={<ResourceView />} />
-      </Route>
-    </Routes>
-  )
-
 }
 
 export default App
