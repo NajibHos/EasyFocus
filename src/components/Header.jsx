673e865f131c560feda58af437cbdbb3384d1a70
Najib Hossain <najibhossn@gmail.com>
2025-06-30 17:02:28 +0600

    Add toggle button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,22 @@
+import { useTheme } from "../utils/ThemeProvider";
 import { useAuth } from "../utils/AuthProvider";
 import { Link, useNavigate } from "react-router-dom";
-import { Github, House, LogIn, LogOut } from "lucide-react";
+import { Github, House, LogIn, LogOut, Moon, Sun } from "lucide-react";
 
 const Header = () => {
 
+  const {theme, toggleTheme } = useTheme();
   const { user, logOutUser } = useAuth();
   const navigate = useNavigate();
 
   return (
      <div className="h-[10vh] w-full flex justify-center
-      items-center">
+      items-center dark:bg-dark-bg">
       <div className="h-full w-[90%] flex justify-between items-center">
         <div className="h-auto w-[40%] flex justify-start items-center">
           <Link to='/'>
-          <h2 className="text-3xl text-white font-semibold
-           cursor-pointer font-logo font-heading">
+          <h2 className="text-3xl  font-semibold font-logo font-heading text-zinc-900
+          dark:text-white cursor-pointer">
             Easy
           <span className="italic">Focus</span>
           </h2>
@@ -27,6 +29,25 @@ const Header = () => {
             !user ? (
 
               <>
+
+              {
+                theme === 'dark' && <button type="button"
+                className="text-lg px-4 py-2  rounded-xl cursor-pointer
+                bg-transparent text-white hover:bg-stone-800"
+                onClick={toggleTheme} >
+                  <Sun />
+                </button>
+              }
+
+              {
+                theme === 'light' && <button type="button"
+                className="text-lg px-4 py-2  rounded-xl cursor-pointer
+                bg-transparent text-zinc-900 hover:text-white hover:bg-stone-800"
+                onClick={toggleTheme} >
+                  <Moon />
+                </button>
+              }
+
               <a href="https://github.com/NajibHos/EasyFocus"
                target="_blank"
                className="text-lg text-zinc-200 cursor-pointer
@@ -61,6 +82,25 @@ const Header = () => {
             ) : (
 
               <>
+
+                {
+                theme === 'dark' && <button type="button"
+                className="text-lg px-4 py-2  rounded-xl cursor-pointer
+                bg-transparent text-white hover:bg-stone-800"
+                onClick={toggleTheme} >
+                  <Sun />
+                </button>
+              }
+
+              {
+                theme === 'light' && <button type="button"
+                className="text-lg px-4 py-2  rounded-xl cursor-pointer
+                bg-transparent text-zinc-900 hover:text-white hover:bg-stone-800"
+                onClick={toggleTheme} >
+                  <Moon />
+                </button>
+              }
+
                 {/* 2 different buttons for smaller and larger screens */}
                 <button type="button"
                 className="block md:max-2xl:hidden
