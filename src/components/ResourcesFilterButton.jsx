d30d9331b576f05a8065cfe6bd0bc9afaee0b372
Najib Hossain <najibhossn@gmail.com>
2025-07-04 16:21:14 +0600

    Update elements style for light mode

--- src/components/ResourcesFilterButton.jsx
+++ src/components/ResourcesFilterButton.jsx
@@ -5,13 +5,16 @@ const ResourcesFilterButton = ({data, activeFilter, setActiveFilter, action}) =>
 
   return (
     <button type="button" className={`text-base font-medium
-     font-description border rounded border-zinc-900
+     font-description border rounded border-stone-300 dark:border-zinc-900
      w-auto px-4 py-2 cursor-pointer
-     ${isActive ? 'text-zinc-950 bg-white' : 'bg-zinc-950 text-white'}`}
+     ${isActive ?
+      ('text-white bg-stone-900 dark:text-zinc-950 dark:bg-white'
+      ) : ('text-zinc-900 bg-transparent dark:bg-zinc-950 dark:text-white')}`}
      onClick={() => {
       setActiveFilter(data.title);
       action(data.title);
-     }} >
+     }}
+    >
       {data.title}
     </button>
   )
