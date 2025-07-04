878eb90b01cf2291fb47c7fca07660bbf436dd6f
Najib Hossain <najibhossn@gmail.com>
2025-07-04 16:34:41 +0600

    Update skeleton style for light mode

--- src/components/ViewPageSkeleton.jsx
+++ src/components/ViewPageSkeleton.jsx
@@ -1,12 +1,12 @@
 
 const NotePageSkeleton = () => {
   return (
-    <div className="h-[90vh] w-full flex justify-center items-center">
+    <div className="h-[90vh] w-full flex justify-center items-center dark:bg-dark-bg">
       <div className="h-full w-full lg:max-2xl:w-[60%]
        flex flex-col justify-center items-center gap-6">
-        <div className="skeleton h-6 w-full"></div>
-        <div className="skeleton h-6 w-full"></div>
-        <div className="skeleton h-6 w-full"></div>
+        <div className="skeleton h-6 w-full bg-zinc-200 dark:bg-zinc-900"></div>
+        <div className="skeleton h-6 w-full bg-zinc-200 dark:bg-zinc-900"></div>
+        <div className="skeleton h-6 w-full bg-zinc-200 dark:bg-zinc-900"></div>
       </div>
     </div>
   )
