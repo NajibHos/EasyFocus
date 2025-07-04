8c6b66042ed3b187dc67e30f4c4de84adbd2dd9d
Najib Hossain <najibhossn@gmail.com>
2025-07-04 14:51:36 +0600

    Update skeleton background color for light mode

--- src/components/TodoSkeleton.jsx
+++ src/components/TodoSkeleton.jsx
@@ -3,9 +3,12 @@ const TodoSkeleton = () => {
   return (
     <>
     <div className="h-[200px] w-full flex flex-col justify-center items-center gap-5">
-      <div className="skeleton h-5 w-full md:w-[80%] lg:w-[60%] bg-zinc-900"></div>
-      <div className="skeleton h-5 w-full md:w-[80%] lg:w-[60%] bg-zinc-900"></div>
-      <div className="skeleton h-5 w-full md:w-[80%] lg:w-[60%] bg-zinc-900"></div>
+      <div className="skeleton h-5 w-full md:w-[80%] lg:w-[60%] bg-zinc-200
+       dark:bg-zinc-900"></div>
+      <div className="skeleton h-5 w-full md:w-[80%] lg:w-[60%] bg-zinc-200
+       dark:bg-zinc-900"></div>
+      <div className="skeleton h-5 w-full md:w-[80%] lg:w-[60%] bg-zinc-200
+       dark:bg-zinc-900"></div>
     </div>
     </>
   )
