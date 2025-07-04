0d81be4b61b84d3b0b7b412871de2073ccd89759
Najib Hossain <najibhossn@gmail.com>
2025-07-04 15:15:10 +0600

    Update background color for light mode

--- src/components/CardsSkeleton.jsx
+++ src/components/CardsSkeleton.jsx
@@ -2,12 +2,18 @@
 const CardsSkeleton = () => {
   return (
       <>
-      <div className="skeleton h-[160px] w-full bg-zinc-900"></div>
-      <div className="skeleton h-[160px] w-full bg-zinc-900"></div>
-      <div className="skeleton h-[160px] w-full bg-zinc-900"></div>
-      <div className="skeleton h-[160px] w-full bg-zinc-900"></div>
-      <div className="skeleton h-[160px] w-full bg-zinc-900"></div>
-      <div className="skeleton h-[160px] w-full bg-zinc-900"></div>
+      <div className="skeleton h-[160px] w-full bg-zinc-200
+       dark:bg-zinc-900"></div>
+      <div className="skeleton h-[160px] w-full bg-zinc-200
+       dark:bg-zinc-900"></div>
+      <div className="skeleton h-[160px] w-full bg-zinc-200
+       dark:bg-zinc-900"></div>
+      <div className="skeleton h-[160px] w-full bg-zinc-200
+       dark:bg-zinc-900"></div>
+      <div className="skeleton h-[160px] w-full bg-zinc-200
+       dark:bg-zinc-900"></div>
+      <div className="skeleton h-[160px] w-full bg-zinc-200
+       dark:bg-zinc-900"></div>
       </>
   )
 }
