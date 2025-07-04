0bb50ff8e51789988d91e2e87be274fda0605899
Najib Hossain <najibhossn@gmail.com>
2025-07-04 15:44:40 +0600

    Update container style with display grid

--- src/components/CardsSkeleton.jsx
+++ src/components/CardsSkeleton.jsx
@@ -1,7 +1,8 @@
 
 const CardsSkeleton = () => {
   return (
-      <>
+      <div className="h-auto w-full grid grid-cols-1 md:grid-cols-2
+       lg:grid-cols-3 gap-8">
       <div className="skeleton h-[160px] w-full bg-zinc-200
        dark:bg-zinc-900"></div>
       <div className="skeleton h-[160px] w-full bg-zinc-200
@@ -14,7 +15,7 @@ const CardsSkeleton = () => {
        dark:bg-zinc-900"></div>
       <div className="skeleton h-[160px] w-full bg-zinc-200
        dark:bg-zinc-900"></div>
-      </>
+      </div>
   )
 }
 
