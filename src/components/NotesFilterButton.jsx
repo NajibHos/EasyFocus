f7c305af2111f6724e93e3f925a3ad317e92ef78
Najib Hossain <najibhossn@gmail.com>
2025-07-04 15:04:46 +0600

    Update button style for light mode

--- src/components/NotesFilterButton.jsx
+++ src/components/NotesFilterButton.jsx
@@ -5,9 +5,11 @@ const NotesFilterButton = ({data, activeFilter, setActiveFilter, action}) => {
 
   return (
     <button type="button" className={`text-base font-medium
-     font-description border rounded border-zinc-900
+     font-description border rounded border-white dark:border-zinc-900
      w-auto px-4 py-2 cursor-pointer
-     ${isActive ? 'text-zinc-950 bg-white' : 'bg-zinc-950 text-white'}`}
+     ${isActive ?
+      ('text-white bg-stone-900 dark:text-zinc-950 dark:bg-white') : (
+      'bg-zinc-600 dark:bg-zinc-950 text-white')}`}
      onClick={() => {
       setActiveFilter(data.title); // assigning activeFilter variable
       action(data.title); // invoking the getFilteredNotes function
