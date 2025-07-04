3fcf9ab6360d17d270dcb4b7c4ec67d8df554ae1
Najib Hossain <najibhossn@gmail.com>
2025-07-04 16:30:48 +0600

    Update elements style for light mode

--- src/pages/NoteView.jsx
+++ src/pages/NoteView.jsx
@@ -43,13 +43,13 @@ const NoteView = () => {
   }
 
   return (
-    <div className="h-[90vh] w-full flex justify-center items-center">
+    <div className="h-[90vh] w-full flex justify-center items-center dark:bg-dark-bg">
       <div className="h-[80%] w-[90%] flex flex-col justify-start
        items-center gap-16 lg:max-2xl:gap-12 overflow-auto">
         <div className="h-auto w-full text-left">
-          <button type="button" className="px-4 py-2 bg-white
-            text-base font-medium font-description rounded
-            text-zinc-950 cursor-pointer"
+          <button type="button" className="px-4 py-2
+            text-base font-medium font-description rounded cursor-pointer
+            text-white bg-stone-900 dark:text-zinc-900 dark:bg-white"
             onClick={() => {
               navigate('/notes')
             }} >
@@ -58,13 +58,13 @@ const NoteView = () => {
         </div>
         <div className="h-auto w-full text-left">
           <h2 className="text-lg font-description font-medium
-          text-zinc-200">
+          text-zinc-900 dark:text-zinc-200">
             {note.title}
           </h2>
         </div>
         <div className="h-auto w-full text-left">
           <h2 className="text-base font-description font-medium
-          text-zinc-400 whitespace-pre-wrap">
+          text-zinc-800 dark:text-zinc-400 whitespace-pre-wrap">
             {note.description}
           </h2>
         </div>
