4f748624c8f3798654d08831c967da94ee601e5e
Najib Hossain <najibhossn@gmail.com>
2025-07-04 16:20:13 +0600

    Update elements style for light mode

--- src/components/ResourceCards.jsx
+++ src/components/ResourceCards.jsx
@@ -7,8 +7,8 @@ const ResourceCards = ({data, setActiveFilter, removeResource}) => {
 
   return (
     <div className="h-[160px] w-full  flex flex-col justify-center
-     items-center gap-5 p-6 border rounded bg-zinc-950
-      border-zinc-900" >
+     items-center gap-5 p-6 border rounded bg-transparent dark:bg-zinc-950
+     border-stone-300 dark:border-zinc-900" >
       <div className="h-auto w-full flex flex-col justify-center
        items-center gap-2">
         <div className="h-auto w-full text-left cursor-pointer"
@@ -18,7 +18,7 @@ const ResourceCards = ({data, setActiveFilter, removeResource}) => {
           );
         }} >
           <h2 className="text-lg font-medium font-description
-          text-zinc-200 truncate hover:underline">
+          text-zinc-900 dark:text-zinc-200 truncate hover:underline">
             {data.title}
           </h2>
         </div>
@@ -39,11 +39,11 @@ const ResourceCards = ({data, setActiveFilter, removeResource}) => {
         <div className="h-auto w-auto">
           <button type="button" className="cursor-pointer"
           onClick={() => {
-            navigate(
+             navigate(
             `/resource-view/${data.$id}` // dynamic route with document id
             )
           }} >
-            <ExternalLink size={22} className="text-blue-600" />
+            <ExternalLink size={24} className="text-blue-600 hover:text-blue-700" />
           </button>
         </div>
         <div className="h-auto w-auto">
@@ -52,7 +52,7 @@ const ResourceCards = ({data, setActiveFilter, removeResource}) => {
             removeResource(data.$id, data.category); // removing doc
             setActiveFilter(data.category); // assigning note category
           }}>
-            <Trash2 size={22} className="text-red-600" />
+            <Trash2 size={22} className="text-red-600 hover:text-red-700" />
           </button>
         </div>
       </div>
