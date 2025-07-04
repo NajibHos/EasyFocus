1d603acb3fcff70db1f8234f9189fe2ad6bf3bab
Najib Hossain <najibhossn@gmail.com>
2025-07-04 15:16:04 +0600

    Update background color for light mode

--- src/components/FilterButtonSkeleton.jsx
+++ src/components/FilterButtonSkeleton.jsx
@@ -2,9 +2,12 @@
 const FilterButtonSkeleton = () => {
   return (
     <div className="h-[40px] w-full flex justify-between items-center">
-      <div className="skeleton h-6 w-[32%] bg-zinc-900"></div>
-      <div className="skeleton h-6 w-[32%] bg-zinc-900"></div>
-      <div className="skeleton h-6 w-[32%] bg-zinc-900"></div>
+      <div className="skeleton h-6 w-[32%] bg-zinc-200
+       dark:bg-zinc-900"></div>
+      <div className="skeleton h-6 w-[32%] bg-zinc-200
+       dark:bg-zinc-900"></div>
+      <div className="skeleton h-6 w-[32%] bg-zinc-200
+       dark:bg-zinc-900"></div>
     </div>
   )
 }
